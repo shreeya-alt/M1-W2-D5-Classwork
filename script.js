9f5419f7f1e41c6cb864c12aad16f2a9e1e7b6a3
shreeya-alt <shreeyamaskey.1@gmail.com>
2022-06-13 22:28:59 +0545

    This is classwork submitted for Day 5

---
+++ script.js
@@ -0,0 +1,21 @@
+function experience(){
+    var drp = document.querySelector('.abt-drop-content');
+    drp.classList.toggle ('show');
+}
+
+function everything(){
+    var content = document.querySelector('.for-everything').classList;
+    alert(content);
+}
+
+function art(){
+    
+}
+
+function creative(){
+    
+}
+
+function design(){
+    
+}
